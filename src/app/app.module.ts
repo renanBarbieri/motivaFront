b078b3642c5a662e71858d06cf8fb60cca2ee94c
Renan Barbieri <renan.hbarbieri@gmail.com>
2017-10-15 20:01:37 -0200

    Criando base do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/homeController';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
---
+++ src/app/components/home/homeController.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './homeView.html',
+  styleUrls: ['./homeStyle.css']
+})
+export class HomeComponent {
+  title = 'Home Component';
+}
