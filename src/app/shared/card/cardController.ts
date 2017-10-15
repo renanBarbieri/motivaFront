251e7b39aaf7ebb7a3e4fdaec9c2f6486f31d17e
Renan Barbieri <renan.hbarbieri@gmail.com>
2017-10-15 20:39:13 -0200

    Melhorando organização de informação do card

--- src/app/shared/card/cardController.ts
+++ src/app/shared/card/cardController.ts
@@ -9,8 +9,8 @@ export class CardComponent {
   thumbnail = 'https://source.unsplash.com/random/800x600';
   avatar = 'https://source.unsplash.com/random/200x200';
   title = 'Título do artigo';
-  subtitle = 'Subtitulo do artigo';
+  subtitle = 'por João da Silva';
   starCount = 2;
   favCount = 5;
-  actionName = 'Ler';
+  actionName = '15/10/2017';
 }
