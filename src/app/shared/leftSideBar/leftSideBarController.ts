33ded9f360841c1fc6b988e4f1f77ccd5616ba85
Renan Barbieri <renan.hbarbieri@gmail.com>
2017-10-23 23:00:31 -0200

    Adicionando menus

--- src/app/shared/leftSideBar/leftSideBarController.ts
+++ src/app/shared/leftSideBar/leftSideBarController.ts
@@ -6,4 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./leftSideBarStyle.css']
 })
 export class LeftSideBarComponent {
+  menus = [
+    {
+      name: 'Meu Perfil',
+      icon: 'account_circle',
+    },
+    {
+      name: 'Configurações',
+      icon: 'settings',
+    },
+    {
+      name: 'Favoritos',
+      icon: 'stars',
+    }
+  ];
+
+  folders = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    }
+  ];
 }
