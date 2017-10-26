d8612ea2e0757a82bd33c1d9989e4b1111554cf8
Renan Barbieri <renan.hbarbieri@gmail.com>
2017-10-26 18:13:11 -0200

    Reorganizando arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { HomeComponent } from './components/home/homeController';
-import { ToolbarComponent } from './shared/toolbar/toolbarController';
-import { CardComponent } from './shared/card/cardController';
-import { TopicArticlesComponent } from './shared/topicArticles/topicArticlesController';
-import { LeftSideBarComponent } from './shared/leftSideBar/leftSideBarController';
+import { HomeComponent } from './pages/home/homeController';
+import { ToolbarComponent } from './components/toolbar/toolbarController';
+import { CardComponent } from './components/card/cardController';
+import { TopicArticlesComponent } from './components/topicArticles/topicArticlesController';
+import { LeftSideBarComponent } from './components/leftSideBar/leftSideBarController';
 
 import { 
   MatCardModule, 
--- src/app/components/leftSideBar/leftSideBarController.ts
+++ src/app/components/leftSideBar/leftSideBarController.ts
@@ -12,27 +12,12 @@ export class LeftSideBarComponent {
       icon: 'account_circle',
     },
     {
-      name: 'Configurações',
-      icon: 'settings',
-    },
-    {
       name: 'Favoritos',
       icon: 'stars',
-    }
-  ];
-
-  folders = [
-    {
-      name: 'Photos',
-      updated: new Date('1/1/16'),
     },
     {
-      name: 'Recipes',
-      updated: new Date('1/17/16'),
-    },
-    {
-      name: 'Work',
-      updated: new Date('1/28/16'),
+      name: 'Configurações',
+      icon: 'settings',
     }
   ];
 }
